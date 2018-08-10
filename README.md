388ddfc67e212f477804a2063f6c87b284416c6d
David Alvarez-Melis <dalvmel@mit.edu>
2018-08-10 18:29:48 -0400

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # mce
 Multistep Contrsative Explanations
+
+
+Download Pretrained Models
+
+```
+wget  people.csail.mit.edu/davidam/MCE/checkpoints/hasy/classif.pth -P src/hasy/
+
+wget  people.csail.mit.edu/davidam/MCE/checkpoints/hasy/mask_model_10x10.pth -P src/hasy/
+
+```
---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(name='mce',
+      version='0.1',
+      description='Multi-step Contrastive Explanations',
+      url='http://github.com/dmelis/mce',
+      author='David Alvarez Melis',
+      author_email='dalvmel@mit.edu',
+      license='MIT',
+      packages = ['mce']
+      #packages= find_packages(exclude=['js', 'node_modules', 'tests']),
+      install_requires=[
+          'numpy',
+          'scipy',
+          'matplotlib',
+          'tqdm',
+          'scikit-learn>=0.18',
+          'scikit-image>=0.12'
+      ],
+      include_package_data=True,
+      zip_safe=False
+)
